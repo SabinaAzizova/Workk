4469ab63f0c774f0ac44a73690b0f0777f838926
Reshid-505 <hreshid2016@gmail.com>
2024-04-02 09:26:10 +0400

    first

---
+++ Bullet.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Bullet
+    {
+        public int CapacityBullet;
+        public int CurrentBullet;
+        public bool Firemode;
+        public string Shoot;
+        public void _shoot(int FireMode)
+        {
+            if ( FireMode==1)
+            {
+                for (int i = 0; i < CapacityBullet; i++) 
+
+                return  Shoot;
+            }
+        }
+      
+            public bool _firemode
+        {
+            get
+            {
+                return true;
+            }
+
+
+
+            set
+            {
+                if (_firemode == Avtomatik)
+                {
+                   _firemode = value;
+               }
+           }
+        }
+        public static (GetRemainBulletCount(int num));
+
+        int GetRemainBulletCount()
+        {
+           static string GetRemainBulletCount = "";
+
+            int GetRemainBulletCount = CapaticyBullet - CurrentBullet;
+                    if(CurrentBullet= CapaticyBullet)
+                    return GetRemainBulletCount;
+                else
+                {
+                    GetRemainBulletCount++;
+                    //return true
+                }
+            }
+        }
+
+
+
+
+    }
+
+
+
+//public void Shoot()
+//            if (FireMode)
+
+//    for (i = 0, i < FireMode.Length, i++)
+//int i = 0;
+                //for (int j = 0; j < CapacityBullet; j++)
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            do
+            {
+                Console.WriteLine("informasiya");
+                Console.WriteLine("atesh achin");
+                Console.WriteLine("getRemainBullet uchun");
+                Console.WriteLine("Reload methodu uchun");
+                Console.WriteLine("ChangeFire methodu uchun");
+                Console.WriteLine("Programi dayandirin");
+                string input = Console.ReadLine();
+                Bullet bullet = new Bullet();
+
+                switch (input)
+                {
+                    case "1"
+                        
+                       
+                }
+            }
+        }
+    }
+}
